01012da17ac5afdbaab0c27a3ca1f1d6b5acd36a
khyej <wl159753@naver.com>
2025-10-27 20:09:07 +0900

    test: 자동차 경주 통합 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -13,6 +14,7 @@ class ApplicationTest extends NsTest {
     private static final int STOP = 3;
 
     @Test
+    @DisplayName("자동차 경주 정상 실행 및 단독 우승자 출력")
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
             () -> {
@@ -24,10 +26,32 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @DisplayName("자동차 경주 정상 실행 및 공동 우승자 출력")
+    void carRacingCowinnerTest(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름 설정 예외 처리")
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                 .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수 설정 예외 처리")
+    void playCountException(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,java", "t"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
